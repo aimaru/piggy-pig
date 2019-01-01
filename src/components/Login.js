b04837ea7d40f509fcd5ef828a9910ea8434ee31
Aiko M <minmilera@gmail.com>
2019-01-01 20:02:36 +0100

    Create Login component (js &  scss)

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import PropTypes from 'prop-types';
+// import App from './App';
+
+export default class Login extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>      
+    
+    );
+  }
+
+}

af5222b063afa954a605d280130ac3573c6b893a
Aiko M <minmilera@gmail.com>
2019-01-01 20:05:14 +0100

    Add router (index.js)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './components/App/App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter} from 'react-router-dom';
+// import { Router } from './Router';
+// import registerServiceWorker from './registerServiceWorker';
 
+
+
+// Storeを作るときに必要
+// import { Provider } from 'react-redux';
+// import createStore from './createReduxStore';
+
+// const store = createStore();
+
+// ReactDOM.render(
+// <Provider store={store}>
+//   <App />
+//   </Provider>,
+//   document.getElementById('root'));
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+      </BrowserRouter>,
+      document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA

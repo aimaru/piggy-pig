2ad16fff10b939e266c0aac842709c35244188e1
Aiko M <minmilera@gmail.com>
2019-01-03 11:24:42 +0100

    Move the files to another folders

--- src/components/AppHolder/Login.js
+++ src/components/AppHolder/Login.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 // import PropTypes from 'prop-types';
 // import App from './App';
+import Header from './Header';
+import SideMenu from './SideMenu';
+
+
 
 export default class Login extends React.Component {
 
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <Header />
+        <SideMenu />
+        
+   
       </div>      
     
     );

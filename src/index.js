2638d1ec5f1f198e34cda48cbfef283011d5304c
Aiko M <minmilera@gmail.com>
2019-01-03 11:23:35 +0100

    Move the file to the App folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './components/App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-
 import { BrowserRouter} from 'react-router-dom';
-// import { Router } from './Router';
+
 // import registerServiceWorker from './registerServiceWorker';
 
 

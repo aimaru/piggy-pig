ddda9afc43412b99b727ce578ae80ba2f5db253b
Aiko M <minmilera@gmail.com>
2019-01-09 00:11:47 +0100

    Create BottomMenu component (in progress)

---
+++ src/components/BottomMenu.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import './BottomMenu.scss';
+// import { Thumbnail } from 'react-bootstrap';
+
+// import IconButton from './IconButton';
+import house from './img/house.svg';
+import piggyMenu from './img/piggybank-menu2.svg';
+import getMoney from './img/get-money.svg';
+import logOut from './img/log-out.svg';
+
+// import PropTypes from 'prop-types';
+
+export default class BottomMenu extends React.Component {
+
+  render() {
+    return (
+      <footer className="BottomMenu">
+      <Grid fluid>
+        <Row>
+      <Col xs={3} mdHidden className="BottomMenu">
+        <div>
+          <img src={house} alt="house icon" className="home-icon" />
+          <p>Hem</p>
+        </div>
+        <div>
+          <img src={piggyMenu} alt="piggybank icon" className="piggybank-icon" />
+          <p>Transaktion</p>
+        </div>
+        <div>
+          <img src={getMoney} alt="money icon" className="money-icon" />
+          <p>Uppgifter</p>
+        </div>
+        <div>
+          <img src={logOut} alt="log out icon" className="log-out-icon" />
+          <p>Loggut</p>
+        </div>
+       {/*   <IconButton  
+            // imgClassName={i._id === this.store.active._id ? 'active' : ''}
+            // key={i._id}
+            // props={i}
+            // handleClick={() => {this.store.goToOrganization(o); this.store.getProcesses()} }
+          />
+ */}
+
+      </Col>
+      </Row>
+      </Grid>
+      </footer>
+   
+    );
+  }
+
+}

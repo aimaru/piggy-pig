fb65d084c30d8c4f7dc115a5a5b8f60a57c21aaf
Aiko M <minmilera@gmail.com>
2019-01-14 10:43:18 +0100

    Comment out some links

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,9 +1,11 @@
 import React from "react";
+// import {Link} from 'react-router-dom';
 import { Col, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./SideMenu.scss";
 // import { Thumbnail } from 'react-bootstrap';
 // import PropTypes from 'prop-types';
 
+// import Transaction from './Transaction';
 
 // Icons made by <a href="https://www.flaticon.com/authors/vectors-market">	
 // Icons made by <a href="https://www.flaticon.com/authors/popcorns-arts">
@@ -20,13 +22,15 @@ const SIDEMENU_ICONS = [
     name: "Hem",
     img: house,
     alt: "house icon",
+    // link: "/",
     imgClassName: "house-icon"
   },
   {
     id: 2,
     name: "Transaktion",
     img: transaction,
-    alt: "transaction icon",
+    // alt: "transaction icon",
+    // link: "/transaction",
     imgClassName: "transaction-icon"
   },
   {
@@ -34,6 +38,7 @@ const SIDEMENU_ICONS = [
     name: "Uppgifter",
     img: mission,
     alt: "mission icon",
+    // link: "/mission",
     imgClassName: "mission-icon"
   },
   {
@@ -41,6 +46,7 @@ const SIDEMENU_ICONS = [
     name: "Loggut",
     img: logOut,
     alt: "logout icon",
+    // link: "/logout",
     imgClassName: "logout-icon"
   }
 ];
@@ -54,14 +60,16 @@ const SideMenu = () => (
             <ListGroupItem key={i.id}>
               {/* <ButtonToolbar className="SelectButton"> */}
               {/* <Button  type="button" className={i.buttonClassName}> */}
+               
+             
+              {/* <Link to={i.link}> */}
                 <img
                   className={i.imgClassName}
                   alt={i.alt}
                   src={i.img}
                 />
-                <span>
-                {i.name}
-                </span>
+                <span>{i.name} </span>
+                {/* </Link>  */}
               {/* </Button> */}
             </ListGroupItem>
           );

59d6bbb1a22662feb5f0ae2836934a257cc1ef17
Aiko M <minmilera@gmail.com>
2019-01-16 13:29:49 +0100

    Create Login component (js & scss)

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import "./Login.scss";
 
 // import PropTypes from 'prop-types';
 // import App from './App';
-import Header from '../components/Header';
-import SideMenu from '../components/SideMenu';
-
-
+// import Header from '../components/Header';
+// import SideMenu from '../components/SideMenu';
 
 export default class Login extends React.Component {
-
   render() {
     return (
-      <div>
-        <Header />
-        <SideMenu />
-        
-        
-   
-      </div>      
-    
+      <Col xs={12} className="Login">
+        {/* <Header />
+        <SideMenu /> */}
+        <Col xs={8} xsOffset={2}  className="holder">
+          <p>Det är logga in sidan</p>
+          <p>
+            Gå till nästa sida <span>>>></span> <Link to="/"> Home </Link>
+          </p>
+          </Col>
+          <Col xsOffset={2} /> 
+      </Col>
     );
   }
-
 }

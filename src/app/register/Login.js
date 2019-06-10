f53e2cda7bce843b2287406195d9f65243bfa93f
Aiko M <minmilera@gmail.com>
2019-06-10 11:39:08 +0200

    Added Login page

--- src/app/register/Login.js
+++ src/app/register/Login.js
@@ -1,28 +1,73 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Grid, Row, Col, Tabs, Tab, ButtonToolbar, Button, Checkbox } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+ 
+import FieldGroup from "../common/FieldGroup";
 import "./Login.scss";
+// import "../scss/Login.scss";
+// import "../scss/mixins.scss";
 
 // import PropTypes from 'prop-types';
 // import App from './App';
-// import Header from '../components/Header';
-// import SideMenu from '../components/SideMenu';
 
 export default class Login extends React.Component {
   render() {
     return (
-      <Col xs={12} className="Login">
-        {/* <Header />
-        <SideMenu /> */}
-        <Col xs={8} xsOffset={2}  className="holder">
-          <p>Det är logga in sidan</p>
-          <p>
-            Gå till nästa sida <span>>>></span> <Link to="/"> Home </Link>
-          </p>
-          </Col>
-          <Col xsOffset={2} /> 
-      </Col>
-    );
-  }
-}
+        <div className="Login">
+          <Col xs={10} sm={8} className="login__wrapper">
+            <Grid fluid>
+
+              <Row>
+                  <Col xs={12} className="login__tabs">
+                    <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+
+                      {/* Tab 1 - Login */}
+                      <Tab eventKey={1} title="Logga in">
+                        <Col sm={12}>
+                          <h2>Logga in</h2>
+                          <form>
+                            <FieldGroup
+                              id="formControlsEmail"
+                              type="email"
+                              placeholder="E-post"
+                            />
+                            <FieldGroup
+                              id="formControlsText"
+                              type="password"
+                              placeholder="Lösenord"
+                            />
+                          </form>
+                          <div className="login__forgot-psw">
+                            <Link to="/"> Glömt lösenord? </Link>
+                          </div>
+                          <ButtonToolbar>
+                            <Button type="submit" className="login__button"
+                            // 動作確認するために、onClick にするが、データベースの準備ができたら、onsubmitにする
+                            // onSubmit={e => this.handleSubmit(e)}
+
+                            // onClick={e => this.handleSubmit(e)}
+                            >
+                              Logga in
+                            </Button>
+                          </ButtonToolbar>
+                          <Checkbox checked readOnly className="login__remember-me">
+                            Kom ihåg mig
+                          </Checkbox>
+                        </Col>
+                      </Tab>
+
+                      {/* Tab 2 - Register */}
+                      <Tab eventKey={2} title="Registrera dig">
+                        Registrera dig
+                      </Tab>
+                    </Tabs>
+                  </Col>
+                </Row>
+
+
+                          </Grid>
+                        </Col> 
+                      </div>
+                  );
+                }
+              }

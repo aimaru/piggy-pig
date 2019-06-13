550b1cf71fafa26e5e8e7a63886f6d00acff99d5
Aiko M <minmilera@gmail.com>
2019-06-13 20:51:22 +0200

    Updated BottomMenu

--- src/app/common/BottomMenu.js
+++ src/app/common/BottomMenu.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Col, ListGroup } from 'react-bootstrap';
 import './BottomMenu.scss';
-// import { Thumbnail } from 'react-bootstrap';
 
+// Icons
 import house from './img/house.svg';
 import piggyMenu from './img/piggybank-menu2.svg';
 import getMoney from './img/get-money.svg';
@@ -14,33 +14,33 @@ export default class BottomMenu extends React.Component {
 
   render() {
     return (
-      <footer className="BottomMenu">
-      <Grid fluid>
-        <Row>
-      <Col xs={3} mdHidden lgHidden className="BottomMenu">
-        <div>
-          <img src={house} alt="house icon" className="home-icon" />
-          <p>Hem</p>
-        </div>
-        <div>
-          <img src={piggyMenu} alt="piggybank icon" className="piggybank-icon" />
-          <p>Transaktion</p>
-        </div>
-        <div>
-          <img src={getMoney} alt="money icon" className="money-icon" />
-          <p>Uppgifter</p>
-        </div>
-        <div>
-          <img src={logOut} alt="log out icon" className="log-out-icon" />
-          <p>Loggut</p>
-        </div>
-    
-      </Col>
-      </Row>
-      </Grid>
+      <footer>
+        <Col xs={12} className="BottomMenu">
+          <ListGroup defaultActiveKey="/">
+
+            <ListGroup.Item action href="/">
+              <img src={house} alt="house icon" className="home-icon" />
+              <p>Hem</p>
+            </ListGroup.Item>
+
+            <ListGroup.Item action href="#" >
+              <img src={piggyMenu} alt="piggybank icon" className="piggybank-icon" />
+              <p>Transaktion</p>
+            </ListGroup.Item>
+            
+            <ListGroup.Item action href="#">
+              <img src={getMoney} alt="money icon" className="money-icon" />
+              <p>Uppgifter</p>
+            </ListGroup.Item>
+
+            <ListGroup.Item action href="#">
+              <img src={logOut} alt="log out icon" className="log-out-icon" />
+              <p>Logg ut</p>
+            </ListGroup.Item>
+
+          </ListGroup>
+        </Col>
       </footer>
-   
     );
   }
-
-}
+};
